b5ca29b964b5eb01069fbe9399e73b9b8fdf51e6
empipio <94937507+empipio@users.noreply.github.com>
2022-02-28 21:41:38 +0000

    exports created, classes started

---
+++ index.js
@@ -0,0 +1,13 @@
+//class for each team category
+
+//inquirer for each class? -> prompts on each class page in a function
+
+//main function on index.js and import class prompts from relevant page
+
+//so the main function for running the app will be on index.js
+
+//on each class page there will be a class and a "helper" function
+
+//tests fit in alongside...
+
+//not sure where src file fits into this
